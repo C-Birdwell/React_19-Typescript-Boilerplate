2375779f8ad671f8aa2c736b97184ed25a5199de
Colin Birdwell <Colin.birdwell@paymentdata.com>
2025-05-29 12:57:30 -0400

    added environment file

--- src/pages/ExamplePage.tsx
+++ src/pages/ExamplePage.tsx
@@ -1,9 +1,15 @@
 import { type FC } from "react";
 
 export const ExamplePage: FC = () => {
+  const envFOO = import.meta.env.VITE_API_FOO;
+  const envBAR = import.meta.env.VITE_API_BAR;
+
   return (
     <>
       <p>ExamplePage</p>
+      <p>
+        {envFOO} {envBAR}
+      </p>
     </>
   );
 };
